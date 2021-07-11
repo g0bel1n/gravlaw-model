d6a86e71a5502ea5cc3d5acfe9dfa9568b5393ee
Lucas Saban <lucas.saban@icloud.com>
2021-07-11 16:06:24 +0200

    comments added

--- gravLawModel.py
+++ gravLawModel.py
@@ -5,7 +5,7 @@ import warnings
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from utils import extract, extr, gravity_law
+from utilitaries.utils import extract, extr, gravity_law
 
 plt.style.use("seaborn-pastel")
 sns.set(rc={'figure.figsize': (20, 20)})
@@ -14,37 +14,54 @@ sns.set(rc={'figure.figsize': (20, 20)})
 class GravLawModel:
 
     def __init__(self, country, iso_a3):
+        """
+        :param country: country's name
+        :param iso_a3: iso a3 code. (ex : France -> FRA)
+        """
 
+        # to avoid bothersome warnings :
         warnings.simplefilter(action="ignore", category=FutureWarning)
         warnings.filterwarnings("ignore")
 
         self.iso_a3 = iso_a3
         self.country = country
+        # As Korea was the main country studied, I used this line to avoid typing the whole country name.
         if self.country == "Korea":
             self.country = "Korea,+Republic+of"
-        self.coord = [0 for _ in range(30)]
+        self.coord = []
+        # cntry contains the most populated cities of country
         self.cntry = gpd.read_file(extract(self.country))
 
+        # For France only I managed to get the actual traffic values
         if self.country == "France":
             self.traffic = gpd.read_file(extract("tmja"))
             self.traffic = self.traffic.to_crs(3857)
             assert (len(self.traffic)) > 0, "Erreur lors du chargement des données réelles"
 
         assert len(self.cntry) > 0, "Le nom du pays est erroné"
-
+        # To match contextily coordinates format
         self.cntry = self.cntry.to_crs(3857)
-
+        #  world is the base map on which we will display our flows and cities
         self.world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
+        # Same reason
         self.world = self.world.to_crs(3857)
+        # Some columns are useless. Let's get rid of them.
         self.cntry = self.cntry.sort_values(by=["population"], ascending=False)[["name", "population", "geometry"]]
-
+        # Once again, a line only for Korea
+        # As Jeju Island is not accesible by road from the Korea, it is not pertinent to keep it.
         if country == "Korea":
             self.cntry = self.cntry.drop(self.cntry.loc[self.cntry['name'] == "Jeju City"].index)
+        # We keep the 30 most populated cities
+
         self.cntry = self.cntry[:30]
         self.T = np.zeros((30, 30))
 
     def flow_calculate(self):
+        """
+        Computes the estimated flows between cities
+        :return: None
+        """
         for i in tqdm(range(30)):
             dist = list(self.cntry.distance(self.cntry.iloc[i]["geometry"]))
             for j in range(i + 1, 30):
@@ -55,7 +72,7 @@ class GravLawModel:
                     self.T[i, j] = gravity_law(cit_i[1], cit_j[1], dist[j])
 
         for i in tqdm(range(30)):
-            self.coord[i] = list(np.ravel(self.cntry.iloc[i]["geometry"].bounds[1:-1]))
+            self.coord.append(list(np.ravel(self.cntry.iloc[i]["geometry"].bounds[1:-1])))
 
         maxi, mini = extr(self.T)
         self.T = (self.T - mini) / (maxi - mini)
@@ -63,14 +80,23 @@ class GravLawModel:
         self.cntry["population"] = (self.cntry["population"] - mini) / (maxi - mini)
 
     def save(self):
+        """
+        save the finale file
+        :return: None
+        """
 
+        # To focus on the country
         base = self.world[self.world["iso_a3"] == self.iso_a3].plot(alpha=0)
+        # Plotting the cities proportionaly to their population
         self.cntry.plot(ax=base, markersize=self.cntry["population"] * 1000, color="red", zorder=3, alpha=0.8)
         for i in tqdm(range(0, 30)):
             for j in range(i + 1, 30):
+                # plot the flows
                 base.plot([self.coord[i][1], self.coord[j][1]], [self.coord[i][0], self.coord[j][0]],
-                          zorder=1, color="black", linewidth=(self.T[i, j]*500)**(0.7), alpha=1)
+                          zorder=1, color="black", linewidth=(self.T[i, j]*500) ** 0.7, alpha=1)
+
         if self.country == "France":
             self.traffic.plot(ax=base, zorder=2, linewidth=self.traffic["tmja"] / 10000)
+
         ctx.add_basemap(base)
         plt.savefig("ressources/GravLawModel_{}.png".format(self.country), bbox_inches='tight')
--- utilitaries/utils.py
+++ utilitaries/utils.py
@@ -2,18 +2,32 @@ import os.path
 import urllib
 import zipfile
 
+import numpy as np
+from typing import Tuple
 
-def gravity_law(p_i, p_j, r):
+
+def gravity_law(p_i: float, p_j: float, r: float) -> float:
+    """
+    :param p_i: Population of the city i
+    :param p_j: Population of the city i
+    :param r: Distance beetween city i and  j
+    :return: the flow of traffic estimated according to a Gravity law model.
+    Actually, the "true" result is proportional to this output
+    """
     return p_i * p_j / r ** 2
 
 
-# mat is symmetric
-def extr(mat, nb_cntry=30):
-    if len(mat) < nb_cntry:
-        nb_cntry = len(mat)
+def extr(mat: np.ndarray) -> Tuple[float, float]:
+    """
+    Iterates upon the array once to output max and min
+    :param mat: matrix containing the distance between cities.
+    Must be symmetric and each value  >0
+    :return: the maximum and minimum value of the array
+    """
+    n = len(mat)
     maxi, mini = mat[0][1], mat[0][1]
-    for i in range(nb_cntry):
-        for j in range(i + 1, nb_cntry):
+    for i in range(n):
+        for j in range(i + 1, n):
             if mat[i][j] > maxi:
                 maxi = mat[i][j]
             elif mat[i][j] < mini:
@@ -22,6 +36,10 @@ def extr(mat, nb_cntry=30):
 
 
 def extract(obj):
+    """
+    :param obj: object of the extraction. Is either a country name or "tmja".
+    :return: the path towards the file asked
+    """
     url = None
     if obj == "tmja":
         extract_dir = "../tmja2018"
